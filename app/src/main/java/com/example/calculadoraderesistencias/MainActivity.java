5c422b10dc3ad7f7f422070e72a2fe9915bb6ccb
TheBracham <brayammiran.d@gmail.com>
2020-12-01 19:37:02 -0300

    Otros agregados

--- app/src/main/java/com/example/calculadoraderesistencias/MainActivity.java
+++ app/src/main/java/com/example/calculadoraderesistencias/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.calculadoraderesistencias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
     }
+
+    public void Calcular(View view) {
+        Intent i = new Intent(this, Calcular.class );
+        startActivity(i);
+    }
+
 }
